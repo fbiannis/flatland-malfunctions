776098b88d9673aff616dda5ecbd7e7f016d0a9a
fbiannis <ianniswaldow@gmail.com>
2025-08-02 14:48:41 +0200

    Fixed typing mistakes

--- modules/flatland_simulation.py
+++ modules/flatland_simulation.py
@@ -10,11 +10,12 @@ class FlatlandSimulation:
         self.environment = environment
         self.active_solution: list = convert_asp_actions_to_list(asp_knowledge.asp_solutions[0])
         self.timestep = 0
-        self.malfunctions = []
+        self.malfunctions: dict = {}
         self.logs = []
     
     def execute_timestep(self) -> bool:
-        """Execute one simulation timestep. Returns True if should continue."""        
+        """Execute one simulation timestep. Returns True if should continue."""
+        self._deduct_malfunctions() # Should deduct be here?     
         self._log_agent_action()
         
         _, _, done, info = self.environment.step(self.active_solution[self.timestep])
@@ -37,9 +38,7 @@ class FlatlandSimulation:
             self.logs.append(f"{agent};{self.timestep};{self.environment.agents[agent].position};{direction_map[self.environment.agents[agent].direction]};{state_map[self.environment.agents[agent].state]};{action_map[self.active_solution[self.timestep][agent]]}\n")
 
     def _handle_malfunction(self, info: dict):
-        new_malfunctions: set = self._add_new_malfunctions(info)
-
-        self._deduct_malfunctions() # Should deduct be here?
+        new_malfunctions: dict = self._add_new_malfunctions(info)
         
         if not new_malfunctions:
             return
@@ -52,26 +51,24 @@ class FlatlandSimulation:
         self.asp_knowledge.add_solution(new_solution)
         self.active_solution = convert_asp_actions_to_list(new_solution)
 
-    def _add_new_malfunctions(self, info: dict) -> set:
+    def _add_new_malfunctions(self, info: dict) -> dict:
         malfunctioning_info = info['malfunction']
-        malfunctioning_trains = {train for train, duration in malfunctioning_info.items() if duration > 0}
         existing_malfunctions = {malf[0] for malf in self.malfunctions}
-        new_malfunctions = malfunctioning_trains.difference(existing_malfunctions)
+        new_malfunctions = {train: duration for train, duration in malfunctioning_info.items() if duration > 0 and train not in existing_malfunctions}
 
-        for train in new_malfunctions:
-            self.malfunctions.append((train, malfunctioning_info[train]))
+        for train, duration in new_malfunctions.items():
+            self.malfunctions.setdefault(train, duration)
 
         return new_malfunctions
 
     def _deduct_malfunctions(self):
-        malfunctions_to_remove = []
-        for i, malf in enumerate(self.malfunctions):
-            self.malfunctions[i] = (self.malfunctions[i][0], self.malfunctions[i][1] - 1)
-            if self.malfunctions[i][1] == 0:
-                malfunctions_to_remove.append(i)
-            
-        for i in sorted(malfunctions_to_remove, reverse=True):
-            del self.malfunctions[i]
+        agents_to_remove = []
+        for agent in list(self.malfunctions.keys()):
+            self.malfunctions[agent] -= 1
+            if self.malfunctions[agent] <= 0:
+                agents_to_remove.append(agent)
+        for agent in agents_to_remove:
+            del self.malfunctions[agent]
 
     def _check_malfunction_problematic(self, asp_new_malfuntions: str) -> bool:
         return get_problematic_by_delta(self.asp_knowledge, asp_new_malfuntions)
